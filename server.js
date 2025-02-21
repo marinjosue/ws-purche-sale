ebc96bcddb2006e0280a0e13d795394f28664b0d
Marin Alquinga Josue Isaac <jimarin@espe.edu.ec>
2025-02-21 00:29:21 -0500

    fix: move PORT declaration for clarity and update log message language

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const stockRoutes = require('./routes/stocksRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +13,7 @@ app.use(express.json());
 // Rutas
 app.use('/stocks', stockRoutes);
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Servitor corridor en el puerto ${PORT}`);
 });
